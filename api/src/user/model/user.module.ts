db8821dc8d9b8614105bfb5873422cb81e8653b9
gskrasti <gunta.skrastina@gmail.com>
2023-08-19 17:27:29 +0200

    missing dependancies

--- api/src/user/model/user.module.ts
+++ api/src/user/model/user.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from '../controller/user.controller';
 import { UserService } from '../service/user-service/user.service';
 import { UserEntity } from './user.entity';
+import { UserHelperService } from '../service/user-helper/user-helper.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity])
   ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, UserHelperService]
 })
 export class UserModule {}
