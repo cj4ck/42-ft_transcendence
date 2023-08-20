56ad13f7d11d0058571ae417b15a136cb623ba11
gskrasti <gunta.skrastina@gmail.com>
2023-08-20 16:02:05 +0200

    merge conflicts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,15 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-<<<<<<< HEAD
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-@Module({
-  imports: [
-    ConfigModule.forRoot({isGlobal: true}),
-    TypeOrmModule.forRoot ({
-=======
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserModule } from './user/model/user.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -19,17 +11,13 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(
     {
->>>>>>> users
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true
     }),
-<<<<<<< HEAD
-=======
     UserModule,
     AuthModule
->>>>>>> users
   ],
   controllers: [AppController],
   providers: [AppService],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
->>>>>>> users
 
 @NgModule({
   declarations: [
@@ -17,13 +14,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    HttpClientModule
-=======
     HttpClientModule,
     BrowserAnimationsModule,
     MatSnackBarModule
->>>>>>> users
   ],
   providers: [],
   bootstrap: [AppComponent]
