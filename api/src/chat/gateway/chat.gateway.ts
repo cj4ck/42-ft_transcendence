19a13c156d6e5d1d22f0fc9d021bb7c3dac383d1
gskrasti <gunta.skrastina@gmail.com>
2023-08-21 19:16:28 +0200

    chat rooms

--- api/src/chat/gateway/chat.gateway.ts
+++ api/src/chat/gateway/chat.gateway.ts
@@ -4,15 +4,15 @@ import { Server, Socket } from 'socket.io';
 import { AuthService } from 'src/auth/service/auth.service';
 import { UserI } from 'src/user/model/user.interface';
 import { UserService } from 'src/user/service/user-service/user.service';
+import { RoomService } from '../service/room-service/room/room.service';
+import { RoomI } from '../model/room.interface';
 
 @WebSocketGateway({cors: {origin: ['https://hoppscotch.io', 'http://localhost:4200', 'http://127.0.0.1:4200', 'http://localhost:3000', 'http://127.0.0.1:3000']}})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
-  
-  title: string[] = [];
 
-  constructor(private authService: AuthService, private userService: UserService) {}
+  constructor(private authService: AuthService, private userService: UserService, private roomService: RoomService) {}
 
   async handleConnection(socket: Socket) {
     try {
@@ -21,8 +21,10 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
       if (!user) {
         return this.disconnect(socket);
       } else {
-        this.title.push('Value ' + Math.random().toString());
-        this.server.emit('message', this.title);
+        socket.data.user = user;
+        const rooms = await this.roomService.getRoomsForUser(user.id, {page: 1, limit: 10});
+
+        return this.server.to(socket.id).emit('rooms', rooms);
       }
       } catch {
         return this.disconnect(socket);
@@ -36,5 +38,10 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private disconnect(socket: Socket) {
     socket.emit('Error', new UnauthorizedException());
     socket.disconnect();
+  }
+
+  @SubscribeMessage('createRoom')
+  async onCreateRoom(socket: Socket, room: RoomI): Promise<RoomI> {
+    return this.roomService.createRoom(room, socket.data.user);
   }
 }
--- frontend/src/app/private/services/chat-service/chat.service.ts
+++ frontend/src/app/private/services/chat-service/chat.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CustomSocket } from '../../sockets/custom-socket'
+import { RoomI, RoomPaginateI } from 'src/app/model/room.interface';
+import { UserI } from 'src/app/model/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,22 @@ export class ChatService {
 
   getMessage() {
     return this.socket.fromEvent('message');
+  }
+
+  getMyRooms() {
+    return this.socket.fromEvent<RoomPaginateI>('rooms');
+  }
+
+  createRoom() {
+    const user2: UserI = {
+      id: 2
+    };
+
+    const room: RoomI = {
+      name: 'Testroom',
+      users: [user2]
+    }
+
+    this.socket.emit('createRoom', room);
   }
 }
