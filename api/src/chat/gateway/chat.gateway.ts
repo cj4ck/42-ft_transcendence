144ae0866c84d4acb132898a6c0b4b8117f98670
gskrasti <gunta.skrastina@gmail.com>
2023-08-21 20:33:16 +0200

    chat room pagination

--- api/src/chat/gateway/chat.gateway.ts
+++ api/src/chat/gateway/chat.gateway.ts
@@ -6,6 +6,7 @@ import { UserI } from 'src/user/model/user.interface';
 import { UserService } from 'src/user/service/user-service/user.service';
 import { RoomService } from '../service/room-service/room/room.service';
 import { RoomI } from '../model/room.interface';
+import { PageI } from '../model/page.interface'; 
 
 @WebSocketGateway({cors: {origin: ['https://hoppscotch.io', 'http://localhost:4200', 'http://127.0.0.1:4200', 'http://localhost:3000', 'http://127.0.0.1:3000']}})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -23,7 +24,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
       } else {
         socket.data.user = user;
         const rooms = await this.roomService.getRoomsForUser(user.id, {page: 1, limit: 10});
-
+        rooms.meta.currentPage = rooms.meta.currentPage - 1;
         return this.server.to(socket.id).emit('rooms', rooms);
       }
       } catch {
@@ -43,5 +44,14 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @SubscribeMessage('createRoom')
   async onCreateRoom(socket: Socket, room: RoomI): Promise<RoomI> {
     return this.roomService.createRoom(room, socket.data.user);
+  }
+
+  @SubscribeMessage('paginateRooms')
+  async onPaginateRoom(socket: Socket, page: PageI) {
+    page.limit = page.limit > 100 ? 100 : page.limit;
+    page.page = page.page + 1;
+    const rooms = await this.roomService.getRoomsForUser(socket.data.user.id, page);
+    rooms.meta.currentPage = rooms.meta.currentPage - 1;
+    return this.server.to(socket.id).emit('rooms', rooms);
   }
 }
