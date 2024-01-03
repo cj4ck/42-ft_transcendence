ddbaed9709e2a678b3d5981e58d1d11c7e8f88e8
Gunta Skrastina <gunta.skrastina@gmail.com>
2024-01-03 18:22:14 +0100

    update fresh

--- frontend/src/app/private/components/navigation/navigation.component.ts
+++ frontend/src/app/private/components/navigation/navigation.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from 'src/app/public/services/auth-service/auth.service';
 import { CustomSocket } from '../../sockets/custom-socket';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserService } from 'src/app/public/services/user-service/user.service';
 
 @Component({
   selector: 'app-navigation',
@@ -17,25 +18,31 @@ export class NavigationComponent implements OnInit {
     private authService: AuthService,
     private socket: CustomSocket,
     private snackbar: MatSnackBar,
-    private http: HttpClient
+    private http: HttpClient,
+    private userService: UserService
   ) { }
+
 
   private httpOptions: { headers: HttpHeaders } = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
   ngOnInit() {
-    var user = this.authService.getLoggedInUser()
-    if (user.fresh) {
-      this.http.post(`api/users/fresh?id=${user.id}`,
-        {},
-        this.httpOptions
-      ).subscribe()
-      let ref = this.snackbar.open(`Please, change your username and avatar `, 'Go to settings', { duration: 10000, horizontalPosition: 'right', verticalPosition: 'top' });
-      ref.onAction().subscribe(() => {
-        this.router.navigate(['private/user-settings'])
-      });
-    }
+    var user = this.userService.findByID(this.authService.getLoggedInUser().id).subscribe(
+      (user) => {
+        if (user.fresh == true) {
+          this.http.post(`api/users/fresh?id=${user.id}`,
+            {},
+            this.httpOptions
+          ).subscribe()
+          let ref = this.snackbar.open(`Please, change your username and avatar `, 'Go to settings', { duration: 10000, horizontalPosition: 'right', verticalPosition: 'top' });
+          ref.onAction().subscribe(() => {
+            this.router.navigate(['private/user-settings'])
+          });
+        }
+      }
+    )
+    
   }
 
   logout() {
